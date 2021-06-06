a762b68f62d864688fe6f7b55ef8324575a73c5b
devryank <ryansg41@gmail.com>
2021-06-06 13:49:25 +0700

    update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -424,7 +424,7 @@
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h1>Aplikasi Pencatatan Pembayaran SPP</h1>
+                <h1>Aplikasi Catatan Pembayaran SPP</h1>
             </div>
         </div>
     </div>
