4b1a027a4994d8b5d3ddbf68043fb0a322a9cc8a
devryank <ryansg41@gmail.com>
2021-06-06 08:56:37 +0700

    fix model name

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -14,6 +14,6 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        student::factory()->count(10)->create();
+        Student::factory()->count(10)->create();
     }
 }
